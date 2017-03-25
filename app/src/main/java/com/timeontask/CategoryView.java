1e94490025ace47cec372597b3be958ca22b5092
Eric Wang <johnericcw@gmail.com>
2017-03-25 11:04:14 -0400

    Comment out test task code

--- app/src/main/java/com/timeontask/CategoryView.java
+++ app/src/main/java/com/timeontask/CategoryView.java
@@ -13,28 +13,28 @@ public class CategoryView extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_category_view);
-
-		TaskTableConnection dbConnection = new TaskTableConnection(getApplicationContext());
-		Task[] tasks = {
-			new Task("DoTA", "Gaming", new Date().getTime(), new Date().getTime()),
-			new Task("LoL", "Gaming", new Date().getTime(), new Date().getTime()),
-			new Task("Maple Story", "Gaming", new Date().getTime(), new Date().getTime()),
-			new Task("EECS 482", "Class", new Date().getTime(), new Date().getTime()),
-			new Task("EECS 485", "Class", new Date().getTime(), new Date().getTime()),
-			new Task("STATS 413", "Class", new Date().getTime(), new Date().getTime()),
-			new Task("MATH 423", "Class", new Date().getTime(), new Date().getTime()),
-			new Task("TCHNCLCM 300", "Class", new Date().getTime(), new Date().getTime()),
-			new Task("Sleep", "Misc", new Date().getTime(), new Date().getTime())
-		};
-
-		for (Task t : tasks) {
-			dbConnection.addTask(t);
-		}
-
-		List<Task> taskList = dbConnection.getTasks(new Date(6), new Date());
-
-		for (Task t : taskList) {
-			System.out.println(t);
-		}
+//
+//		TaskTableConnection dbConnection = new TaskTableConnection(getApplicationContext());
+//		Task[] tasks = {
+//			new Task("DoTA", "Gaming", new Date().getTime(), new Date().getTime()),
+//			new Task("LoL", "Gaming", new Date().getTime(), new Date().getTime()),
+//			new Task("Maple Story", "Gaming", new Date().getTime(), new Date().getTime()),
+//			new Task("EECS 482", "Class", new Date().getTime(), new Date().getTime()),
+//			new Task("EECS 485", "Class", new Date().getTime(), new Date().getTime()),
+//			new Task("STATS 413", "Class", new Date().getTime(), new Date().getTime()),
+//			new Task("MATH 423", "Class", new Date().getTime(), new Date().getTime()),
+//			new Task("TCHNCLCM 300", "Class", new Date().getTime(), new Date().getTime()),
+//			new Task("Sleep", "Misc", new Date().getTime(), new Date().getTime())
+//		};
+//
+//		for (Task t : tasks) {
+//			dbConnection.addTask(t);
+//		}
+//
+//		List<Task> taskList = dbConnection.getTasks(new Date(6), new Date());
+//
+//		for (Task t : taskList) {
+//			System.out.println(t);
+//		}
 	}
 }
